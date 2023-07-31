69b3f9c55f49ae2d6297cfa9cb87b91b99a9b96c
AntonNov <anton.nov123@gmail.com>
2023-07-31 19:28:58 +0300

    fix: change toReadable function

--- src/index.js
+++ src/index.js
@@ -4,35 +4,35 @@ module.exports = function toReadable(number) {
     const tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     let numberstring = number.toString();
     if (numberstring.length == 1) {
-        return digits[+numberstring[0]]
+        return digits[numberstring[0]]
     }
     if (numberstring.length == 2) {
-        if (+numberstring[0] == 1) {
+        if (numberstring[0] == 1) {
             return tens[numberstring[1]]
         }
-        if (+numberstring[1] == 0) {
+        if (numberstring[1] == 0) {
             return dozens[numberstring[0]]
         }
-        return dozens[+numberstring[0]] + ' ' + digits[numberstring[1]]
+        return dozens[numberstring[0]] + ' ' + digits[numberstring[1]]
     }
     if (numberstring.length == 3) {
-        if (+numberstring[1] == 0 && +numberstring[2] == 0) {
-            return digits[+numberstring[0]] + ' hundred '
+        if (numberstring[1] == 0 && numberstring[2] == 0) {
+            return digits[numberstring[0]] + ' hundred'
         }
-        if (+numberstring[2] == 0) {
-            return digits[+numberstring[0]] + ' hundred '
-                + dozens[+numberstring[1]]
+        if (numberstring[2] == 0) {
+            return digits[numberstring[0]] + ' hundred '
+                + dozens[numberstring[1]]
         }
-        if (+numberstring[1] == 0) {
-            return digits[+numberstring[0]] + ' hundred '
-                + digits[+numberstring[2]]
+        if (numberstring[1] == 0) {
+            return digits[numberstring[0]] + ' hundred '
+                + digits[numberstring[2]]
         }
-        if (+numberstring[1] == 1) {
-            return digits[+numberstring[0]] + ' hundred '
-                + tens[numberstring[1]]
+        if (numberstring[1] == 1) {
+            return digits[numberstring[0]] + ' hundred '
+                + tens[numberstring[2]]
         }
-        return digits[+numberstring[0]] + ' hundred '
-            + dozens[+numberstring[1]] + ' ' + digits[+numberstring[2]]
+        return digits[numberstring[0]] + ' hundred '
+            + dozens[numberstring[1]] + ' ' + digits[numberstring[2]]
     }
 
 }
